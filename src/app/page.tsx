c25f0f3cde4c76ff2f1c4d8b45baeb15a803c6e9
jtorrecilla <rastagar2003@gmail.com>
2025-03-26 21:00:07 +0100

    WIP

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -134,6 +134,8 @@ export default function Home() {
             
             }}> 
             <div style={{
+                         display:'flex',
+                         flexDirection:'row',
                 justifyContent:"space-between",
                 padding:5,
                 width:'100%'
